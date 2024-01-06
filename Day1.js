637b4e1b6beaecfd96a250811e49d62fffa757f4
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2024-01-06 23:21:01 +0530

    array

---
+++ Day1.js
@@ -0,0 +1,24 @@
+// console.log("Hello, Its Day1 to job Switch Preparation. Wish Me Luck");
+// console.log(
+//   "Todays topic: Arrays and String initially if time allows will choose some other topics as well."
+// );
+
+let arr1 = [1, 2, 3, 4];
+// console.log(typeof arr1);
+// console.log(arr1[1]);
+// console.log(arr1["1"]);
+// console.log(arr1(-1));
+// console.log(arr1["-1"]);
+// console.log(arr1."1");
+// console.log(arr1.length);
+// console.log(Object.keys(arr1));
+// console.log(arr1.length);
+// let fruits = [];
+// fruits[5] = "mango";
+// console.log(fruits[5]); // 'mango'
+// console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+// console.log(fruits.length);
+
+// fruits.length = 10;
+// console.log(fruits);
+console.log(arr1[2.5]);
